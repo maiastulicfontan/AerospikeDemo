28f5f62521e4ac5132433dbaf66dba046afea103
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2020-07-15 20:48:21 -0300

    Agregue servicio de usuario, algo de main class

--- src/main/java/com/grupo12/aerospike/Demo.java
+++ src/main/java/com/grupo12/aerospike/Demo.java
@@ -1,6 +1,79 @@
 package com.grupo12.aerospike;
 
+import java.util.Scanner;
+
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.AerospikeException;
+import com.aerospike.client.policy.ClientPolicy;
+import com.grupo12.aerospike.servicios.ServicioUsuario;
+
 public class Demo {
+	private String seedHost;
+	private int port;
+	private AerospikeClient cliente;
+	
+	public Demo () {
+		ClientPolicy clientPolicy = new ClientPolicy();
+		this.cliente = new AerospikeClient (clientPolicy, this.seedHost, this.port);
+	}
+	
+	protected void finalize() throws Throwable{
+		if (this.cliente != null) {
+			this.cliente.close();
+		}
+	}
+	
+	protected void work () {
+		try {
+			System.out.println("INFO: Conectándose a Aerospike");
+			
+			// Estableciendo conexión con Aerospike
+			if (cliente == null || !cliente.isConnected()) {
+				System.out.println("\nERROR: Fallo en la conexión");
+			} else {
+				Scanner input = new Scanner (System.in);
+				System.out.println("\nINFO: Conexión a Aerospike exitosa");
+				
+				//Inicializar servicios
+				ServicioUsuario su = new ServicioUsuario(cliente);
+				
+				// Opciones
+				System.out.println("\nIngrese la acción que desea realizar: \n");
+				System.out.println("1> Crear un usuario y un tweet\n");
+				System.out.println("2> Leer el registro de un usuario\n");
+				System.out.println("0> Salir\n");
+				System.out.println("\nSeleccione 0-5 y presione Enter\n");
+				int seleccion = input.nextInt();
+				
+				if (seleccion != 0) {
+					switch (seleccion){
+						case 1:
+							System.out.println("\n**********Ha seleccionado: crear un usuario y un tweet**********\n"); 
+							//su.crearUsuario();
+							//st.crearTweet();
+							break;
+						case 2:
+							System.out.println("\n**********Ha seleccionado: leer el registro de un usuario**********\n");
+							//su.leerUsuario();
+							break;
+						default:
+							break;
+					}
+				}
+			}
+		} catch (AerospikeException e) {
+			System.out.println("Aerospike Exception - Mensaje: "+e.getMessage()+ "\n");
+			System.out.println("Aerospike Exception - Stack Trace: "+e.getStackTrace()+ "\n");
+		} catch (Exception e) {
+			System.out.println("Exception - Mensaje: "+e.getMessage()+ "\n");
+			System.out.println("Exception - Stack Trace: "+e.getStackTrace()+ "\n");
+		} finally {
+			if (cliente != null && cliente.isConnected()) {
+				cliente.close();
+			}
+			System.out.println("\nINFO: Presione cualquier tecla para salir");
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/main/java/com/grupo12/aerospike/servicios/ServicioUsuario.java
@@ -0,0 +1,58 @@
+package com.grupo12.aerospike.servicios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.AerospikeException;
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
+import com.aerospike.client.policy.RecordExistsAction;
+import com.aerospike.client.policy.WritePolicy;
+
+public class ServicioUsuario {
+	private AerospikeClient cliente;
+	
+	public ServicioUsuario (AerospikeClient cliente) {
+		this.cliente = cliente;
+	}
+	
+	public void crearUsuario() throws AerospikeException {
+		System.out.println("\n**************Crear Usuario**************\n");
+		
+		String nombreUsuario;
+		String password;
+		String genero;
+		String intereses;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Ingrese nombre de usuario: ");
+		nombreUsuario = input.nextLine();
+		
+		if (nombreUsuario != null && nombreUsuario.length() > 0) {
+			System.out.println("Ingrese contraseña para el usuario "+ nombreUsuario + ":");
+			password = input.nextLine();
+			
+			System.out.println("Ingrese genero (M-Masculino, F-Femenino, O-Otro): ");
+			genero = input.nextLine().substring(0, 1);
+			
+			System.out.println("Ingrese intereses separados por comas: ");
+			intereses = input.nextLine();
+			
+			WritePolicy writePolicy = new WritePolicy();
+			writePolicy.recordExistsAction = RecordExistsAction.UPDATE;
+			
+			Key key = new Key("test", "usuarios", nombreUsuario);
+			Bin bin1 = new Bin("nombreusuario", nombreUsuario);
+			Bin bin2 = new Bin("password", password);
+			Bin bin3 = new Bin("genero", genero);
+			Bin bin4 = new Bin("contadorTweets", 0);
+			Bin bin5 = new Bin("intereses", Arrays.asList(intereses.split(",")));
+			
+			cliente.put(writePolicy, key, bin1, bin2, bin3, bin4, bin5);
+			
+			System.out.println("\nINFO: Registro de usuario creado");
+		}
+	}
+}

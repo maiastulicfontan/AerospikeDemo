830040a315de275880d65644a5e0a12f20376af4
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2020-07-15 23:56:55 -0300

    Archivo .csv agregado, inserción desde csv funcionando

--- src/main/java/com/grupo12/aerospike/Demo.java
+++ src/main/java/com/grupo12/aerospike/Demo.java
@@ -14,7 +14,14 @@ public class Demo {
 	
 	public Demo () {
 		ClientPolicy clientPolicy = new ClientPolicy();
-		this.cliente = new AerospikeClient (clientPolicy, this.seedHost, this.port);
+		this.cliente = new AerospikeClient (clientPolicy);
+	}
+	
+	public Demo (String seedHost, int port) {
+		ClientPolicy clientPolicy = new ClientPolicy();
+		this.seedHost = seedHost;
+		this.port = port;
+		this.cliente = new AerospikeClient (clientPolicy, seedHost, port);
 	}
 	
 	protected void finalize() throws Throwable{
@@ -39,8 +46,14 @@ public class Demo {
 				
 				// Opciones
 				System.out.println("\nIngrese la acción que desea realizar: \n");
-				System.out.println("1> Crear un usuario y un tweet\n");
+				System.out.println("1> Cargar usuario manualmente\n");
+				System.out.println("2> Cargar usuarios desde un archivo .csv\n");
 				System.out.println("2> Leer el registro de un usuario\n");
+				System.out.println("3> Consulta interesante 1\n");
+				System.out.println("4> Consulta interesante 2\n");
+				System.out.println("5> Consulta interesante 3\n");
+				System.out.println("6> Consulta interesante 4\n");
+				System.out.println("7> Consulta interesante 5\n");
 				System.out.println("0> Salir\n");
 				System.out.println("\nSeleccione 0-5 y presione Enter\n");
 				int seleccion = input.nextInt();
@@ -48,13 +61,27 @@ public class Demo {
 				if (seleccion != 0) {
 					switch (seleccion){
 						case 1:
-							System.out.println("\n**********Ha seleccionado: crear un usuario y un tweet**********\n"); 
+							System.out.println("\n**********Ha seleccionado: cargar usuario manualmente**********\n"); 
 							//su.crearUsuario();
-							//st.crearTweet();
 							break;
 						case 2:
 							System.out.println("\n**********Ha seleccionado: leer el registro de un usuario**********\n");
 							//su.leerUsuario();
+							break;
+						case 3:
+							System.out.println("\n**********Ha seleccionado: consulta interesante 1**********\n");
+							break;
+						case 4:
+							System.out.println("\n**********Ha seleccionado: consulta interesante 2**********\n");
+							break;
+						case 5:
+							System.out.println("\n**********Ha seleccionado: consulta interesante 3**********\n");
+							break;
+						case 6:
+							System.out.println("\n**********Ha seleccionado: consulta interesante 4**********\n");
+							break;
+						case 7:
+							System.out.println("\n**********Ha seleccionado: consulta interesante 5**********\n");
 							break;
 						default:
 							break;
@@ -75,8 +102,34 @@ public class Demo {
 		}
 	}
 
+	public String getSeedHost() {
+		return seedHost;
+	}
+
+	public void setSeedHost(String seedHost) {
+		this.seedHost = seedHost;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public AerospikeClient getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(AerospikeClient cliente) {
+		this.cliente = cliente;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Demo demo = new Demo("172.28.128.4", 3000);
+		ServicioUsuario su = new ServicioUsuario(demo.getCliente());
+		su.insertarUsuariosDesdeCsv("src/main/resources/usuarios.csv");
 
 	}
 
--- src/main/java/com/grupo12/aerospike/servicios/ServicioUsuario.java
+++ src/main/java/com/grupo12/aerospike/servicios/ServicioUsuario.java
@@ -1,7 +1,14 @@
 package com.grupo12.aerospike.servicios;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.Arrays;
 import java.util.Scanner;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.AerospikeException;
@@ -13,6 +20,10 @@ import com.aerospike.client.policy.WritePolicy;
 public class ServicioUsuario {
 	private AerospikeClient cliente;
 	
+	public ServicioUsuario() {
+		
+	}
+	
 	public ServicioUsuario (AerospikeClient cliente) {
 		this.cliente = cliente;
 	}
@@ -23,6 +34,7 @@ public class ServicioUsuario {
 		String nombreUsuario;
 		String password;
 		String genero;
+		String fechaNacimiento; //cambiar por Date?
 		String intereses;
 		
 		Scanner input = new Scanner(System.in);
@@ -37,26 +49,58 @@ public class ServicioUsuario {
 			System.out.println("Ingrese genero (M-Masculino, F-Femenino, O-Otro): ");
 			genero = input.nextLine().substring(0, 1);
 			
+			System.out.println("Ingrese fecha de nacimiento con formato YYYY-mm-dd: ");
+			fechaNacimiento = input.nextLine();
+			
 			System.out.println("Ingrese intereses separados por comas: ");
 			intereses = input.nextLine();
 			
-			WritePolicy writePolicy = new WritePolicy();
-			writePolicy.recordExistsAction = RecordExistsAction.UPDATE;
-			
-			Key key = new Key("test", "usuarios", nombreUsuario);
-			Bin bin1 = new Bin("nombreusuario", nombreUsuario);
-			Bin bin2 = new Bin("password", password);
-			Bin bin3 = new Bin("genero", genero);
-			Bin bin4 = new Bin("contadortweets", 0);
-			Bin bin5 = new Bin("intereses", Arrays.asList(intereses.split(",")));
-			
-			cliente.put(writePolicy, key, bin1, bin2, bin3, bin4, bin5);
+			insertarUsuario(nombreUsuario, password, genero, fechaNacimiento, 0, intereses);
 			
 			System.out.println("\nINFO: Registro de usuario creado");
 		}
 	}
 	
-	public void insertarUsuario(String nombreUsuario, String password, String genero, int contadortweets, ) {
+	public void insertarUsuario(String nombreUsuario, String password, String genero, String fechaNacimiento, int contadorTweets, String intereses ) {
+		WritePolicy writePolicy = new WritePolicy();
+		writePolicy.recordExistsAction = RecordExistsAction.UPDATE;
 		
+		Key key = new Key("test", "usuarios", nombreUsuario);
+		Bin bin1 = new Bin("nom_usuario", nombreUsuario);
+		Bin bin2 = new Bin("password", password);
+		Bin bin3 = new Bin("genero", genero);
+		Bin bin4 = new Bin("fecha_nac", fechaNacimiento);
+		Bin bin5 = new Bin("cont_tweets", contadorTweets);
+		Bin bin6 = new Bin("intereses", Arrays.asList(intereses.split(",")));
+		
+		cliente.put(writePolicy, key, bin1, bin2, bin3, bin4, bin5, bin6);		
+	}
+	
+	
+	public void insertarUsuariosDesdeCsv (String pathArchivoCsv) {
+		try {
+			Reader in = new FileReader(pathArchivoCsv);
+			Iterable<CSVRecord> registros = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
+			int contadorRegistros = 0;
+			for (CSVRecord registro : registros) {
+				String nombreUsuario = registro.get("nombre_usuario");
+				String password = registro.get("password");
+				String genero = registro.get("genero");
+				String fechaNacimiento = registro.get("fecha_nacimiento");
+				int contadorTweets = Integer.parseInt(registro.get("contador_tweets"));
+				String intereses = registro.get("intereses");
+				insertarUsuario(nombreUsuario, password, genero, fechaNacimiento, contadorTweets, intereses);
+				contadorRegistros++;
+			}
+			System.out.println("Se han insertado "+contadorRegistros+" registros.");
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found exception - Stack Trace: "+e.getStackTrace());
+		} catch (IOException e) {
+			System.out.println("IO exception - Stack Trace: "+e.getStackTrace());
+		}
+	}
+	
+	public void printUsuario(String nombreUsuario, String password, String genero, String fechaNacimiento, int contadorTweets, String intereses ) {
+		System.out.println(nombreUsuario+","+password+","+genero+","+fechaNacimiento+","+contadorTweets+","+intereses);
 	}
 }
